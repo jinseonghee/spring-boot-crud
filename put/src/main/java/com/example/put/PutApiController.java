b71fba753828d99e7d5c824f418f0584aea933d2
jinseonghee <wlstjdgml0610@gmail.com>
2021-08-04 15:02:48 +0900

    PUT API

--- put/src/main/java/com/example/put/PutApiController.java
+++ put/src/main/java/com/example/put/PutApiController.java
@@ -1,18 +1,22 @@
 package com.example.put;
 
 import com.example.put.dto.PostRequestDto;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("/api")
 public class PutApiController {
 
-    @PutMapping("/put")
-    public void put(@RequestBody PostRequestDto requestDto) {
-        System.out.println(requestDto);
+
+    @PutMapping("/put/{userId}")
+    public PostRequestDto put(@RequestBody PostRequestDto requestDto, @PathVariable(name = "userId") Long id) {
+                                        //@PathVariable은 {userId}와 메서드에 써준 변수의 이름과 일치해야 하는데, 그럴 수 없을 경우 Name property를 사용하여 맞춰준다.
+
+        System.out.println(id);
+        //System.out.println(requestDto); //RestController의 경우 그냥 object를 Return 시키면 Spring boot가 이 object를 가지고
+        return requestDto;              // object mapper를 통해 json으로 바꿔준다
+                                        //에코 - 내가 매개변수로 받았던 데이터를 그대로 return되기 때문에 내가 보냈던 데이터가 그대로 return
     }
 }
+
--- put/src/main/java/com/example/put/dto/PostRequestDto.java
+++ put/src/main/java/com/example/put/dto/PostRequestDto.java
@@ -1,11 +1,18 @@
 package com.example.put.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import java.util.List;
 
+//class에 대해서 전체적으로 같은 룰을 적용 시킴.(해당 클래스는 object라는 모듈이 동작 할 때 snakecase로 인식 함)
+@JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class PostRequestDto {
 
     private String name;
     private int age;
+    //@JsonProperty 일괄적으로 한가지 변수에 대해 naming을 지어주는 @
     private List<CarDto> carList;
 
     public String getName() {
